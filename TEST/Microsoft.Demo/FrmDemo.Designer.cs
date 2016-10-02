923576efc6425d0b9c40ab56fb04da1fb6c8e913
TimAndy <xuchonglei@126.com>
2016-10-02 13:12:43 +0800

    测试代码调整

--- TEST/Microsoft.Demo/FrmDemo.Designer.cs
+++ TEST/Microsoft.Demo/FrmDemo.Designer.cs
@@ -78,7 +78,7 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(463, 346);
+            this.button2.Location = new System.Drawing.Point(43, 248);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(75, 23);
             this.button2.TabIndex = 0;
@@ -88,7 +88,7 @@
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(544, 346);
+            this.button3.Location = new System.Drawing.Point(124, 248);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(75, 23);
             this.button3.TabIndex = 0;
