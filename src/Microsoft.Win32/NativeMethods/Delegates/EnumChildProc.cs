cd45f55648d2099bf55b385efa8ef20d807fba25
TimAndy <xuchonglei@126.com>
2021-10-12 16:30:33 +0800

    去掉注释中的空格

--- src/Microsoft.Win32/NativeMethods/Delegates/EnumChildProc.cs
+++ src/Microsoft.Win32/NativeMethods/Delegates/EnumChildProc.cs
@@ -6,17 +6,17 @@ namespace Microsoft.Win32
     public static partial class NativeMethods
     {
         /// <summary>
-        ///     <para>功能:</para>
-        ///     <para>该函数是由应用程序定义的，与函数 EnumChildWindows 一起使用的回调函数。它接收子窗口句柄。</para>
-        ///     <para>类型 WNDENUMPROC 定义了一个指向回调函数的指针。EnumChildProc 是一个应用程序定义的函数名的位置标志符。</para>
-        ///     <para>.</para>
-        ///     <para>备注:</para>
-        ///     <para>回调函数可以执行任何要求的任务。</para>
-        ///     <para>应用程序必须通过将其地址传送给 EnumChildWindows 函数来注册这个回调函数。</para>
+        /// <para>功能:</para>
+        /// <para>该函数是由应用程序定义的，与函数EnumChildWindows一起使用的回调函数。它接收子窗口句柄。</para>
+        /// <para>类型WNDENUMPROC定义了一个指向回调函数的指针。EnumChildProc是一个应用程序定义的函数名的位置标志符。</para>
+        /// <para>.</para>
+        /// <para>备注:</para>
+        /// <para>回调函数可以执行任何要求的任务。</para>
+        /// <para>应用程序必须通过将其地址传送给EnumChildWindows函数来注册这个回调函数。</para>
         /// </summary>
-        /// <param name="hwnd">指向在 EnumChildWindows 中指定的父窗口的子窗口句柄。</param>
-        /// <param name="lParam">指定在 EnumChildWindows 函数给出的应用程序定义值。</param>
-        /// <returns>如要继续枚举，回调函数必须返回 TRUE；如要停止枚举，回调函数必须返回 FALSE。</returns>
+        /// <param name="hwnd">指向在EnumChildWindows中指定的父窗口的子窗口句柄。</param>
+        /// <param name="lParam">指定在EnumChildWindows函数给出的应用程序定义值。</param>
+        /// <returns>如要继续枚举，回调函数必须返回TRUE；如要停止枚举，回调函数必须返回FALSE。</returns>
         public delegate bool EnumChildProc(IntPtr hwnd, object lParam);
     }
 }
--- src/Microsoft.Win32/NativeMethods/Delegates/EnumWindowsProc.cs
+++ src/Microsoft.Win32/NativeMethods/Delegates/EnumWindowsProc.cs
@@ -6,16 +6,16 @@ namespace Microsoft.Win32
     public static partial class NativeMethods
     {
         /// <summary>
-        ///     <para>功能:</para>
-        ///     <para>函数是一个与 EnumWindows 或 EnumDesktopWindows 一起使用的应用程序定义的回调函数。它接收顶层窗口句柄。</para>
-        ///     <para>WNDENUMPROC 定义一个指向这个回调函数的指针。EnumWindowsProc 是应用程序定义函数名的位置标志符。</para>
-        ///     <para>.</para>
-        ///     <para>备注:</para>
-        ///     <para>应用程序必须通过传递给 EnumWindows 或 EnumDesktopWindows 应用程序地址来注册这个回调函数。</para>
+        /// <para>功能:</para>
+        /// <para>函数是一个与EnumWindows或EnumDesktopWindows一起使用的应用程序定义的回调函数。它接收顶层窗口句柄。</para>
+        /// <para>WNDENUMPROC定义一个指向这个回调函数的指针。EnumWindowsProc是应用程序定义函数名的位置标志符。</para>
+        /// <para>.</para>
+        /// <para>备注:</para>
+        /// <para>应用程序必须通过传递给EnumWindows或EnumDesktopWindows应用程序地址来注册这个回调函数。</para>
         /// </summary>
         /// <param name="hwnd">顶层窗口句柄。</param>
-        /// <param name="lParam">指定在 EnumWindows 或 EnumDesktopWindows 中的应用程序定义值。</param>
-        /// <returns>如要继续枚举，回调函数必须返回 TRUE；如要停止枚举，回调函数必须返回 FALSE。</returns>
+        /// <param name="lParam">指定在EnumWindows或EnumDesktopWindows中的应用程序定义值。</param>
+        /// <returns>如要继续枚举，回调函数必须返回TRUE；如要停止枚举，回调函数必须返回FALSE。</returns>
         public delegate bool EnumWindowsProc(IntPtr hwnd, object lParam);
     }
 }
