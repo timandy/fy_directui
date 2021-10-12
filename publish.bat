dea5afc0a9bc39afee8ce6bbfea5b4c770e2cb44
TimAndy <xuchonglei@126.com>
2021-10-12 15:45:59 +0800

    添加发布 win32 的打包脚本

--- publish.bat
+++ publish.bat
@@ -2,4 +2,6 @@ for /f %%a in (VERSION) do (set version=%%a)
 echo %version%
 nuget pack directui.nuspec
 nuget push directui.%version%.nupkg -Source https://api.nuget.org/v3/index.json -ApiKey %1
+nuget pack microsoft.win32.nuspec
+nuget push microsoft.win32.%version%.nupkg -Source https://api.nuget.org/v3/index.json -ApiKey %1
 del *.nupkg /S/Q
